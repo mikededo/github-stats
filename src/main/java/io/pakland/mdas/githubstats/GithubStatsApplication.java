9e417d4f6fdf6f29793cbaa40744b0beb4ffba24
Paco Lozano <plozano@pakland.io>
2022-11-13 03:09:10 +0100

    initial commit

---
+++ src/main/java/io/pakland/mdas/githubstats/GithubStatsApplication.java
@@ -0,0 +1,13 @@
+package io.pakland.mdas.githubstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pakland/mdas/githubstats/GithubStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pakland.mdas.githubstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
