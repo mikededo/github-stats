ba9f199f9cb1c436c0eb07a5ba542da88f4a6a00
Miquel de Domingo <miquelddg@gmail.com>
2022-11-20 09:26:16 +0100

    refactor: remove entity jdoc

--- src/main/java/io/pakland/mdas/githubstats/domain/Organization.java
+++ src/main/java/io/pakland/mdas/githubstats/domain/Organization.java
@@ -10,21 +10,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-/**
- * <b>Class</b>: organization <br/>
- * <b>Copyright</b>: 2022 La Salle - mdas <br/>.
- *
- * @author 2022  MDAS <br/>
- * <u>Service Provider</u>: La Salle <br/>
- * <u>Developed by</u>: mdas <br/>
- * <u>Changes:</u><br/>
- * <ul>
- *   <li>
- *     Septiembre 13, 2022 Creación de Clase.
- *   </li>
- * </ul>
- */
-
 @Data
 @NoArgsConstructor
 @ToString
