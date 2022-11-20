c123bc142eb7075bf8e07f1dde0a15e386508cec
Miquel de Domingo <miquelddg@gmail.com>
2022-11-20 09:29:00 +0100

    refactor: remove unnecesary jdoc from repo

--- src/main/java/io/pakland/mdas/githubstats/domain/repository/IComment.java
+++ src/main/java/io/pakland/mdas/githubstats/domain/repository/IComment.java
@@ -5,18 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * <b>Class</b>: IComment <br/>
- * <b>Copyright</b>: 2022 La Salle - mdas <br/>.
- *
- * @author 2022  MDAS <br/>
- * <u>Service Provider</u>: La Salle <br/>
- * <u>Developed by</u>: mdas <br/>
- * <u>Changes:</u><br/>
- * <ul>
- *   <li>
- *     Septiembre 13, 2022 Creación de Clase.
- *   </li>
- * </ul>
+ * Add jdoc about the rep
  */
 
 @Repository
