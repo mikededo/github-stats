09126956ed61cb4f2953a9c5c9ab0ef21ddac9c6
Miquel de Domingo <miquelddg@gmail.com>
2022-11-22 20:04:26 +0100

    chore!: move gradle config to maven

--- src/test/java/io/pakland/mdas/githubstats/GithubStatsApplicationTests.java
+++ src/test/java/io/pakland/mdas/githubstats/GithubStatsApplicationTests.java
@@ -1,13 +1,18 @@
 package io.pakland.mdas.githubstats;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class GithubStatsApplicationTests {
-
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext ctx) {
+		assertThat(ctx).isNotNull();
 	}
-
 }
