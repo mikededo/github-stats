814293c47c428066c926d9bd87d6405f45b3cb2f
Miquel de Domingo <miquelddg@gmail.com>
2022-11-29 18:21:57 +0100

    test: separate truthy/falsy tests

--- src/test/java/io/pakland/mdas/githubstats/infrastructure/shell/validation/TeamNameValidatorTest.java
+++ src/test/java/io/pakland/mdas/githubstats/infrastructure/shell/validation/TeamNameValidatorTest.java
@@ -6,17 +6,19 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TeamNameValidatorTest {
+    TeamNameValidator teamNameValidator = new TeamNameValidator();
 
     @Test
-    void testValidation() {
-        TeamNameValidator teamNameValidator = new TeamNameValidator();
-
+    void testValidationShouldPass() {
         assertTrue(teamNameValidator.validate("name"));
         assertTrue(teamNameValidator.validate("test-hello-world"));
         assertTrue(teamNameValidator.validate("22312"));
         assertTrue(teamNameValidator.validate("alphanumeric-and-hyphens-23"));
         assertTrue(teamNameValidator.validate("UPPERcase"));
+    }
 
+    @Test
+    void testValidationShouldFail() {
         assertFalse(teamNameValidator.validate("name with spaces"));
         assertFalse(teamNameValidator.validate("very-very-very-very-very-very-very-very-very-very-long-name"));
         assertFalse(teamNameValidator.validate("-name"));
