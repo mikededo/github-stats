2da3d2cedd351c43701b6a592425a821bb394b52
Miquel de Domingo <miquelddg@gmail.com>
2022-11-30 08:29:59 +0100

    refactor: import cleanup

--- src/main/java/io/pakland/mdas/githubstats/domain/model/Organization.java
+++ src/main/java/io/pakland/mdas/githubstats/domain/model/Organization.java
@@ -7,9 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Data
 @NoArgsConstructor
