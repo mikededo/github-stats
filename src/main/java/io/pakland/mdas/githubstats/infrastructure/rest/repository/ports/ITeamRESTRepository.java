9ba22b35a5bce5715a74e88313bbfc1e082175be
Adrià Manero <adriamanero97@gmail.com>
2022-12-07 16:46:49 +0100

    fix: changes after rebase onto main. Handle REST repositories HTTP Exceptions

--- src/main/java/io/pakland/mdas/githubstats/infrastructure/rest/repository/ports/ITeamRESTRepository.java
+++ src/main/java/io/pakland/mdas/githubstats/infrastructure/rest/repository/ports/ITeamRESTRepository.java
@@ -1,9 +1,10 @@
 package io.pakland.mdas.githubstats.infrastructure.rest.repository.ports;
 
 import io.pakland.mdas.githubstats.application.dto.TeamDTO;
+import io.pakland.mdas.githubstats.application.exceptions.HttpException;
 
 import java.util.List;
 
 public interface ITeamRESTRepository {
-    List<TeamDTO> fetchTeamsFromOrganization(String organizationName);
+    List<TeamDTO> fetchTeamsFromOrganization(String organizationName) throws HttpException;
 }
--- src/main/java/io/pakland/mdas/githubstats/infrastructure/rest/repository/ports/IUserRESTRepository.java
+++ src/main/java/io/pakland/mdas/githubstats/infrastructure/rest/repository/ports/IUserRESTRepository.java
@@ -1,9 +1,10 @@
 package io.pakland.mdas.githubstats.infrastructure.rest.repository.ports;
 
 import io.pakland.mdas.githubstats.application.dto.UserDTO;
+import io.pakland.mdas.githubstats.application.exceptions.HttpException;
 
 import java.util.List;
 
 public interface IUserRESTRepository {
-    List<UserDTO> fetchUsersFromTeam(String organizationName, String teamSlug);
+    List<UserDTO> fetchUsersFromTeam(String organizationName, String teamSlug) throws HttpException;
 }
