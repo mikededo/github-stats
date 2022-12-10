6b36825ac4dd07886d7a542f28a99b0f0691d2b3
Adrià Manero <adriamanero97@gmail.com>
2022-12-10 13:51:47 +0100

    fix: remove the OrganizationDTO and use only our domain entity

--- src/main/java/io/pakland/mdas/githubstats/domain/repository/OrganizationExternalRepository.java
+++ src/main/java/io/pakland/mdas/githubstats/domain/repository/OrganizationExternalRepository.java
@@ -1,10 +1,10 @@
 package io.pakland.mdas.githubstats.domain.repository;
 
-import io.pakland.mdas.githubstats.application.dto.OrganizationDTO;
 import io.pakland.mdas.githubstats.application.exceptions.HttpException;
+import io.pakland.mdas.githubstats.domain.Organization;
 
 import java.util.List;
 
 public interface OrganizationExternalRepository {
-    List<OrganizationDTO> fetchAvailableOrganizations() throws HttpException;
+    List<Organization> fetchAvailableOrganizations() throws HttpException;
 }
