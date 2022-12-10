66dcc94f2a21445efbb11161460b72f141e4de04
Adrià Manero <adriamanero97@gmail.com>
2022-12-10 12:37:03 +0100

    fix: refactor controller entities for DTOs

--- src/main/java/io/pakland/mdas/githubstats/application/dto/OrganizationDTO.java
+++ src/main/java/io/pakland/mdas/githubstats/application/dto/OrganizationDTO.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,4 +16,6 @@ public class OrganizationDTO {
 
     @JsonProperty("login")
     private String login;
+
+    private List<TeamDTO> teams;
 }
