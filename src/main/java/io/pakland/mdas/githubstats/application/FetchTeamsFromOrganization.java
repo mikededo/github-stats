77a07ffb738a16849943002105eb7af22617ab14
Paco Lozano <plozano@pakland.io>
2022-12-11 18:10:06 +0100

    fix: add missing @service spring annotation

--- src/main/java/io/pakland/mdas/githubstats/application/FetchTeamsFromOrganization.java
+++ src/main/java/io/pakland/mdas/githubstats/application/FetchTeamsFromOrganization.java
@@ -3,9 +3,11 @@ package io.pakland.mdas.githubstats.application;
 import io.pakland.mdas.githubstats.application.exceptions.HttpException;
 import io.pakland.mdas.githubstats.domain.Team;
 import io.pakland.mdas.githubstats.domain.repository.TeamExternalRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class FetchTeamsFromOrganization {
     private final TeamExternalRepository teamExternalRepository;
 
