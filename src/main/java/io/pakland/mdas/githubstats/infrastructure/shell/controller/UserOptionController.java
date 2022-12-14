529b7aebc6f7bf0ab34b69d903dd7639037b9ef5
Miquel de Domingo <miquelddg@gmail.com>
2022-12-14 22:58:00 +0100

    refactor: replace `for:each` for class methods
    We currently had many nested for each in order to loop over the
    different entities and construct our organization. It had reached a
    point in which it was really nested, lots of indentation and it was not
    sustaniable/scalable.

--- src/main/java/io/pakland/mdas/githubstats/infrastructure/shell/controller/UserOptionController.java
+++ src/main/java/io/pakland/mdas/githubstats/infrastructure/shell/controller/UserOptionController.java
@@ -7,19 +7,17 @@ import io.pakland.mdas.githubstats.domain.*;
 import io.pakland.mdas.githubstats.domain.repository.*;
 import io.pakland.mdas.githubstats.infrastructure.github.repository.*;
 import io.pakland.mdas.githubstats.infrastructure.shell.model.UserOptionRequest;
+import java.util.List;
 import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @NoArgsConstructor
 public class UserOptionController {
 
     Logger logger = LoggerFactory.getLogger(UserOptionController.class);
-    private UserOptionRequest userOptionRequest;
     private OrganizationExternalRepository organizationExternalRepository;
     private TeamExternalRepository teamExternalRepository;
     private UserExternalRepository userExternalRepository;
@@ -28,14 +26,15 @@ public class UserOptionController {
     private CommitExternalRepository commitExternalRepository;
 
     public UserOptionController(UserOptionRequest userOptionRequest) {
-        this.userOptionRequest = userOptionRequest;
         WebClientConfiguration webClientConfiguration = new WebClientConfiguration(
-                "https://api.github.com", userOptionRequest.getApiKey());
-        this.organizationExternalRepository = new OrganizationGitHubRepository(webClientConfiguration);
+            "https://api.github.com", userOptionRequest.getApiKey());
+        this.organizationExternalRepository = new OrganizationGitHubRepository(
+            webClientConfiguration);
         this.teamExternalRepository = new TeamGitHubRepository(webClientConfiguration);
         this.userExternalRepository = new UserGitHubRepository(webClientConfiguration);
         this.repositoryExternalRepository = new RepositoryGitHubRepository(webClientConfiguration);
-        this.pullRequestExternalRepository = new PullRequestGitHubRepository(webClientConfiguration);
+        this.pullRequestExternalRepository = new PullRequestGitHubRepository(
+            webClientConfiguration);
         this.commitExternalRepository = new CommitGitHubRepository(webClientConfiguration);
     }
 
@@ -44,54 +43,90 @@ public class UserOptionController {
             // TODO: If the execution succeeds, we should make an entry to the historic_queries table.
             // Fetch the API key's available organizations.
             List<Organization> organizationList = new FetchAvailableOrganizations(
-                    this.organizationExternalRepository)
-                    .execute();
-            // Start building the github-stats relational schema.
-            for (Organization organization : organizationList) {
-                // Fetch the teams belonging to the available organization.
-                List<Team> teamList = new FetchTeamsFromOrganization(teamExternalRepository)
-                        .execute(organization.getLogin());
+                this.organizationExternalRepository)
+                .execute();
+            organizationList.forEach(this::fetchTeamsFromOrganization);
+        } catch (HttpException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-                for (Team team : teamList) {
-                    organization.addTeam(team);
-                    // Fetch the members of each team.
-                    List<User> userList = new FetchUsersFromTeam(userExternalRepository)
-                            .execute(organization.getLogin(), team.getSlug());
-                    // Fetch the repositories for each team.
-                    List<Repository> repositoryList = new FetchRepositoriesFromTeam(
-                            repositoryExternalRepository)
-                            .execute(organization.getLogin(), team.getSlug());
-                    // Add the team to the repository
-                    repositoryList.forEach(r -> r.setTeam(team));
+    private void fetchTeamsFromOrganization(Organization organization) {
+        try {
+            List<Team> teamList = new FetchTeamsFromOrganization(teamExternalRepository)
+                .execute(organization.getLogin());
+            teamList.forEach(team -> {
+                this.fetchRepositoriesFromTeam(organization, team);
+                this.fetchUsersFromTeam(organization, team);
+            });
+        } catch (HttpException e) {
+            throw new RuntimeException(e);
+        }
 
-                    for (Repository repository : repositoryList) {
-                        team.addRepository(repository);
-                        // Fetch pull requests from each team.
-                        List<PullRequest> pullRequestList = new FetchPullRequestsFromRepository(
-                                pullRequestExternalRepository)
-                                .execute(repository.getOwnerLogin(), repository.getName());
+    }
 
-                        for (PullRequest pullRequest : pullRequestList) {
-                            // Add the repository to the pull request
-                            pullRequest.setRepository(repository);
-                            /*
-                                TODO: if the user of the PR belongs to the team, increment the prs executed inside the team,
-                                TODO: else increment the prs executed outside the team.
-                            */
-                            // TODO: Save for later calculate the additions, deletions and commit num. from PR aggregation.
-                            List<Commit> commitList = new FetchCommitsFromPullRequest(commitExternalRepository)
-                                    .execute(repository.getOwnerLogin(), repository.getName(), pullRequest.getNumber());
-                            for (Commit commit : commitList) {
-                                // TODO: Fetch PR reviews.
+    private void fetchRepositoriesFromTeam(Organization organization, Team team) {
+        organization.addTeam(team);
+        try {
+            // Fetch the repositories for each team.
+            List<Repository> repositoryList = new FetchRepositoriesFromTeam(
+                repositoryExternalRepository)
+                .execute(organization.getLogin(), team.getSlug());
+            // Add the team to the repository
+            repositoryList.forEach(repository -> {
+                repository.setTeam(team);
+                this.fetchPullRequestsFromRepository(team, repository);
+            });
+        } catch (HttpException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-                            }
-                        }
+    private void fetchUsersFromTeam(Organization organization, Team team) {
+        try {
+            // Fetch the members of each team.
+            List<User> userList = new FetchUsersFromTeam(userExternalRepository)
+                .execute(organization.getLogin(), team.getSlug());
 
-                        repository.setPullRequests(pullRequestList);
-                    }
+            team.setUsers(userList);
+        } catch (HttpException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-                    team.setUsers(userList);
-                }
+    private void fetchPullRequestsFromRepository(Team team, Repository repository) {
+        team.addRepository(repository);
+        try {
+            // Fetch pull requests from each team.
+            List<PullRequest> pullRequestList = new FetchPullRequestsFromRepository(
+                pullRequestExternalRepository)
+                .execute(repository.getOwnerLogin(), repository.getName());
+
+            pullRequestList.forEach(pullRequest -> this.fetchCommitsFromPullRequest(repository, pullRequest));
+            repository.setPullRequests(pullRequestList);
+        } catch (HttpException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private void fetchCommitsFromPullRequest(Repository repository, PullRequest pullRequest) {
+        // Add the repository to the pull request
+        pullRequest.setRepository(repository);
+        /*
+        TODO: if the user of the PR belongs to the team, increment the prs executed inside the team
+        TODO: else increment the prs executed outside the team
+        TODO: Save for later calculate the additions, deletions and commit num. from PR aggregation
+        */
+        //
+        try {
+            List<Commit> commitList = new FetchCommitsFromPullRequest(
+                commitExternalRepository)
+                .execute(repository.getOwnerLogin(), repository.getName(),
+                    pullRequest.getNumber());
+            for (Commit commit : commitList) {
+                // TODO: Fetch PR reviews.
+
             }
         } catch (HttpException e) {
             throw new RuntimeException(e);
