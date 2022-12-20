446d10f87c4439c5a619a926da8ab5bc78610d63
Sergi Domingo <sdomingobasora@gmail.com>
2022-12-20 18:55:25 +0100

    feat: add PullRequestAggregation

---
+++ src/main/java/io/pakland/mdas/githubstats/domain/entity/PullRequestAggregation.java
@@ -0,0 +1,17 @@
+package io.pakland.mdas.githubstats.domain.entity;
+
+import java.util.List;
+
+public class PullRequestAggregation {
+
+    public PullRequestAggregation aggregate(List<PullRequest> pullRequests) {
+        // TODO
+        return new PullRequestAggregation();
+    }
+
+    public String toCSV() {
+        // TODO
+        return "";
+    }
+
+}
