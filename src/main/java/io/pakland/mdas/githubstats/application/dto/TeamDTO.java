b71a8b2682ca7ce5b282cffd12ad6fafbcba45e4
Miquel de Domingo <miquelddg@gmail.com>
2022-12-22 08:18:52 +0100

    feat: added `TeamDTO` interface

---
+++ src/main/java/io/pakland/mdas/githubstats/application/dto/TeamDTO.java
@@ -0,0 +1,6 @@
+package io.pakland.mdas.githubstats.application.dto;
+
+public interface TeamDTO {
+    Integer getId();
+    String getSlug();
+}
--- src/main/java/io/pakland/mdas/githubstats/infrastructure/github/model/GitHubTeamDTO.java
+++ src/main/java/io/pakland/mdas/githubstats/infrastructure/github/model/GitHubTeamDTO.java
@@ -1,11 +1,22 @@
 package io.pakland.mdas.githubstats.infrastructure.github.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.pakland.mdas.githubstats.application.dto.TeamDTO;
 
-public class GitHubTeamDTO {
+public class GitHubTeamDTO implements TeamDTO {
     @JsonProperty("id")
     private Integer id;
 
     @JsonProperty("slug")
     private String slug;
+
+    @Override
+    public Integer getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getSlug() {
+        return this.slug;
+    }
 }
