7b3cfae0596d1d88b1751cf6a548bcce53263ff9
Miquel de Domingo <miquelddg@gmail.com>
2022-12-22 08:36:12 +0100

    feat: added `PullRequestDTO` interface

--- src/main/java/io/pakland/mdas/githubstats/infrastructure/github/model/GitHubPullRequestDTO.java
+++ src/main/java/io/pakland/mdas/githubstats/infrastructure/github/model/GitHubPullRequestDTO.java
@@ -1,9 +1,10 @@
 package io.pakland.mdas.githubstats.infrastructure.github.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.pakland.mdas.githubstats.application.dto.PullRequestDTO;
 import io.pakland.mdas.githubstats.domain.entity.PullRequestState;
 
-public class GitHubPullRequestDTO {
+public class GitHubPullRequestDTO implements PullRequestDTO {
 
     @JsonProperty("id")
     private Integer id;
@@ -13,5 +14,20 @@ public class GitHubPullRequestDTO {
 
     @JsonProperty("state")
     private PullRequestState state;
+
+    @Override
+    public Integer getId() {
+        return this.id;
+    }
+
+    @Override
+    public Integer getNumber() {
+        return this.number;
+    }
+
+    @Override
+    public PullRequestState getState() {
+        return this.state;
+    }
 
 }
