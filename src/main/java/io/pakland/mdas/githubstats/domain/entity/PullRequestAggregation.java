245ff8e396306e86d41e680474ff467965adb0ef
Adrià Manero <adriamanero97@gmail.com>
2022-12-23 16:27:53 +0100

    fix: refactor CommitAggregation to move lines counters to PullRequestAggregation

--- src/main/java/io/pakland/mdas/githubstats/domain/entity/PullRequestAggregation.java
+++ src/main/java/io/pakland/mdas/githubstats/domain/entity/PullRequestAggregation.java
@@ -3,6 +3,17 @@ package io.pakland.mdas.githubstats.domain.entity;
 import java.util.List;
 
 public class PullRequestAggregation implements CSVExportable {
+    private int linesAdded;
+
+    private int linesRemoved;
+
+    public int getLinesAdded() {
+        return linesAdded;
+    }
+
+    public int getLinesRemoved() {
+        return linesRemoved;
+    }
 
     public PullRequestAggregation aggregate(List<PullRequest> pullRequests) {
         // TODO
