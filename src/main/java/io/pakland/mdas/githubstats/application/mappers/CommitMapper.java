b1eb97c699ed6e4cfc2cca2d298ade2e46b70f96
Adrià Manero <adriamanero97@gmail.com>
2022-12-23 08:56:35 +0100

    fix: additions and deletions should be obtained from PRs

---
+++ src/main/java/io/pakland/mdas/githubstats/application/mappers/CommitMapper.java
@@ -0,0 +1,17 @@
+package io.pakland.mdas.githubstats.application.mappers;
+
+import io.pakland.mdas.githubstats.application.dto.CommitDTO;
+import io.pakland.mdas.githubstats.domain.entity.Commit;
+
+public class CommitMapper {
+
+    public static Commit dtoToEntity(CommitDTO dto) {
+        return Commit.builder()
+            .sha(dto.getSha())
+            .user(dto.getUser())
+            .additions(dto.getAdditions())
+            .deletions(dto.getDeletions())
+            .date(dto.getDate())
+            .build();
+    }
+}
--- src/main/java/io/pakland/mdas/githubstats/domain/entity/Commit.java
+++ src/main/java/io/pakland/mdas/githubstats/domain/entity/Commit.java
@@ -1,47 +1,28 @@
 package io.pakland.mdas.githubstats.domain.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.Objects;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-import javax.persistence.*;
-import java.time.Instant;
 import java.util.Date;
-import java.util.Map;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @ToString
+@AllArgsConstructor
+@Builder
 public class Commit {
 
-    @JsonProperty("sha")
     private String sha;
+
+    private String user;
+
+    private Integer additions;
+
+    private Integer deletions;
 
     private Date date;
 
-    private User user;
-
     private PullRequest pullRequest;
 
-    private int additions;
-
-    private int deletions;
-
-    @JsonProperty("commit")
-    private void unpackDateFromNestedObject(Map<String, Object> commitJson) {
-        Map<String, Object> committer = (Map<String, Object>)commitJson.get("committer");
-        this.date = Date.from(Instant.parse(committer.get("date").toString()));
-    }
-
-    @JsonProperty("stats")
-    private void setAdditionDeletionsLines(Map<String, Object> stats) {
-        additions = Integer.parseInt(stats.get("additions").toString());
-        deletions = Integer.parseInt(stats.get("deletions").toString());
-    }
-
-    // The hashcde must be overridden otherwise it causes a stakoverflow issue
+    // The hashcode must be overridden otherwise it causes a stackoverflow issue
     // when adding a commit to a set like structure
     @Override
     public int hashCode() {
