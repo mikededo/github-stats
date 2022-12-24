9bd3e88ab65d1d3286a914cf3c64e2d3fb55bfc5
Miquel de Domingo <miquelddg@gmail.com>
2022-12-24 09:12:29 +0100

    fix: minor pr corrections

--- src/main/java/io/pakland/mdas/githubstats/infrastructure/controller/MainController.java
+++ src/main/java/io/pakland/mdas/githubstats/infrastructure/controller/MainController.java
@@ -4,19 +4,14 @@ import io.pakland.mdas.githubstats.infrastructure.github.controller.GitHubUserMi
 import io.pakland.mdas.githubstats.infrastructure.postgres.PostgresMiddleware;
 import io.pakland.mdas.githubstats.infrastructure.shell.model.ShellRequest;
 
-import java.sql.SQLOutput;
-
 public class MainController {
 
     private final Middleware middleware;
 
-    private final ShellRequest request;
-
     public MainController(ShellRequest request) {
-        this.request = request;
         middleware = Middleware.link(
-                new PostgresMiddleware(request),
-                new GitHubUserMiddleware(request)
+            new PostgresMiddleware(request),
+            new GitHubUserMiddleware(request)
         );
     }
 
