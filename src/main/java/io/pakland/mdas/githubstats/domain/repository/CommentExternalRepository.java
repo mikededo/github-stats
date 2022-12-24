472b2e8d4c4d51419f939fec83ea1962b9fc6245
Adrià Manero <adriamanero97@gmail.com>
2022-12-24 13:11:06 +0100

    fix: refactor comment use case and GitHubRepository

--- src/main/java/io/pakland/mdas/githubstats/domain/repository/CommentExternalRepository.java
+++ src/main/java/io/pakland/mdas/githubstats/domain/repository/CommentExternalRepository.java
@@ -2,10 +2,26 @@ package io.pakland.mdas.githubstats.domain.repository;
 
 import io.pakland.mdas.githubstats.application.exceptions.HttpException;
 import io.pakland.mdas.githubstats.domain.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 public interface CommentExternalRepository {
-    public List<Comment> fetchCommentsFromPullRequest(
-            String repositoryOwner, String repositoryName, Integer pullRequestNumber, Integer page, Integer perPage) throws HttpException;
+
+    public List<Comment> fetchCommentsFromPullRequest(FetchCommentsFromPullRequestRequest request) throws HttpException;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    public static class FetchCommentsFromPullRequestRequest {
+        private String repositoryOwner;
+        private String repositoryName;
+        private Integer pullRequestNumber;
+        private Integer page;
+        private Integer perPage;
+    }
 }
