4f5574a99a250cb85b5d9392b965f0763072ad4c
Paco Lozano <plozano@pakland.io>
2022-12-24 04:26:20 +0100

    refactor!: remove jpa annotations from non persistent entities

--- src/main/java/io/pakland/mdas/githubstats/domain/entity/Repository.java
+++ src/main/java/io/pakland/mdas/githubstats/domain/entity/Repository.java
@@ -13,30 +13,18 @@ import lombok.ToString;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 @Builder
-@Table(name = "repository")
 public class Repository {
 
-  @Id
-  @Column(updatable = false, nullable = false)
   private Integer id;
 
-  @Column(name = "name")
   private String name;
 
-  @Column(name = "owner_login")
   private String ownerLogin;
 
-  @ManyToOne(fetch = FetchType.LAZY)
   @ToString.Exclude
   private Team team;
 
-  @OneToMany(
-    mappedBy = "repository",
-    cascade = CascadeType.ALL,
-    orphanRemoval = true
-  )
   private Set<PullRequest> pullRequests = new HashSet<>();
 
   public void addPullRequests(Collection<PullRequest> pullRequests) {
--- src/main/java/io/pakland/mdas/githubstats/domain/entity/UserReview.java
+++ src/main/java/io/pakland/mdas/githubstats/domain/entity/UserReview.java
@@ -12,25 +12,14 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @ToString
-@Entity
-@Table(name = "user_review")
 public class UserReview {
 
-  @Id
-  @Column(updatable = false, nullable = false)
   private Integer id;
 
-  @ManyToOne(fetch = FetchType.LAZY)
   private User user;
 
-  @ManyToOne(fetch = FetchType.LAZY)
   private PullRequest pullRequest;
 
-  @OneToMany(
-    mappedBy = "userReview",
-    cascade = CascadeType.ALL,
-    orphanRemoval = true
-  )
   private List<Comment> comments = new ArrayList<>();
 
   public int sumCommentLength() {
