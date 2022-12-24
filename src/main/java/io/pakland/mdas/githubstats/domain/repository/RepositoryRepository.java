dab222b1713ade36c5e1ecf47301d3259f3113f3
Paco Lozano <plozano@pakland.io>
2022-12-24 04:23:29 +0100

    refactor!: remove unneeded use cases and repositories

--- src/main/java/io/pakland/mdas/githubstats/domain/repository/RepositoryRepository.java
+++
@@ -1,8 +0,0 @@
-package io.pakland.mdas.githubstats.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryRepository extends JpaRepository<io.pakland.mdas.githubstats.domain.entity.Repository, Integer> {
-}
