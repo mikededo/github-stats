acf7823743ae1c6cd7c219e5c6d9a88998385b12
Miquel de Domingo <miquelddg@gmail.com>
2022-12-25 20:11:38 +0100

    test: fix tests

--- src/test/java/io/pakland/mdas/githubstats/application/mappers/PullRequestMapperTest.java
+++ src/test/java/io/pakland/mdas/githubstats/application/mappers/PullRequestMapperTest.java
@@ -3,6 +3,7 @@ package io.pakland.mdas.githubstats.application.mappers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import io.pakland.mdas.githubstats.application.dto.PullRequestDTO;
+import io.pakland.mdas.githubstats.application.dto.UserDTO;
 import io.pakland.mdas.githubstats.domain.entity.PullRequest;
 import io.pakland.mdas.githubstats.domain.enums.PullRequestState;
 import org.junit.jupiter.api.Test;
@@ -16,12 +17,18 @@ public class PullRequestMapperTest {
         Mockito.when(dto.getId()).thenReturn(1);
         Mockito.when(dto.getNumber()).thenReturn(1);
         Mockito.when(dto.getState()).thenReturn(PullRequestState.ALL);
+        UserDTO userDTO = Mockito.mock(UserDTO.class);
+        Mockito.when(userDTO.getId()).thenReturn(1);
+        Mockito.when(userDTO.getLogin()).thenReturn("mikededo");
+        Mockito.when(dto.getUser()).thenReturn(userDTO);
 
         PullRequest entity = PullRequestMapper.dtoToEntity(dto);
 
         assertEquals(1, entity.getId());
         assertEquals(1, entity.getNumber());
         assertEquals(PullRequestState.ALL, entity.getState());
+        assertEquals(entity.getUser().getId(), 1);
+        assertEquals(entity.getUser().getLogin(), "mikededo");
     }
 
 }
