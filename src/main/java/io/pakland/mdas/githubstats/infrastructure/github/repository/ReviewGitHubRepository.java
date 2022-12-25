dcc7cf2d05bb6ecfc990b940d23a77c0bd1cdc1c
Miquel de Domingo <miquelddg@gmail.com>
2022-12-25 08:14:33 +0100

    feat: add internal caching on fetching user reviews

--- src/main/java/io/pakland/mdas/githubstats/infrastructure/github/repository/ReviewGitHubRepository.java
+++ src/main/java/io/pakland/mdas/githubstats/infrastructure/github/repository/ReviewGitHubRepository.java
@@ -3,39 +3,51 @@ package io.pakland.mdas.githubstats.infrastructure.github.repository;
 import io.pakland.mdas.githubstats.application.exceptions.HttpException;
 import io.pakland.mdas.githubstats.application.mappers.ReviewMapper;
 import io.pakland.mdas.githubstats.domain.entity.UserReview;
+import io.pakland.mdas.githubstats.domain.lib.InternalCaching;
 import io.pakland.mdas.githubstats.domain.repository.ReviewExternalRepository;
 import io.pakland.mdas.githubstats.infrastructure.github.model.GitHubReviewDTO;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
-import java.util.List;
-
 public class ReviewGitHubRepository implements ReviewExternalRepository {
+
     private final WebClientConfiguration webClientConfiguration;
     private final Logger logger = LoggerFactory.getLogger(ReviewGitHubRepository.class);
+
+    private final InternalCaching<String, List<UserReview>> cache = new InternalCaching<>();
 
     public ReviewGitHubRepository(WebClientConfiguration webClientConfiguration) {
         this.webClientConfiguration = webClientConfiguration;
     }
 
     @Override
-    public List<UserReview> fetchReviewsFromPullRequest(FetchReviewsFromPullRequestRequest request) throws HttpException {
+    public List<UserReview> fetchReviewsFromPullRequest(FetchReviewsFromPullRequestRequest request)
+        throws HttpException {
+        String query = String.format("/repos/%s/%s/pulls/%s/reviews?%s",
+            request.getRepositoryOwner(),
+            request.getRepositoryName(),
+            request.getPullRequestNumber(),
+            getRequestParams(request.getPage(), request.getPerPage())
+        );
+        List<UserReview> maybeResult = cache.get(query);
+        if (maybeResult != null) {
+            return maybeResult;
+        }
+
         try {
 
-            return this.webClientConfiguration.getWebClient().get()
-                    .uri(String.format("/repos/%s/%s/pulls/%s/reviews?%s",
-                            request.getRepositoryOwner(),
-                            request.getRepositoryName(),
-                            request.getPullRequestNumber(),
-                            getRequestParams(request.getPage(), request.getPerPage())
-                    ))
-                    .retrieve()
-                    .bodyToFlux(GitHubReviewDTO.class)
-                    .map(ReviewMapper::dtoToEntity)
-                    .collectList()
-                    .block();
+            List<UserReview> result = this.webClientConfiguration.getWebClient().get()
+                .uri(query)
+                .retrieve()
+                .bodyToFlux(GitHubReviewDTO.class)
+                .map(ReviewMapper::dtoToEntity)
+                .collectList()
+                .block();
+            cache.add(query, result);
 
+            return result;
         } catch (WebClientResponseException ex) {
             logger.error(ex.toString());
             throw new HttpException(ex.getRawStatusCode(), ex.getMessage());
