02438ab65ea762b4d6276fdb51e39120d126e06b
Adrià Manero <adriamanero97@gmail.com>
2022-12-26 21:43:46 +0100

    feat: refactor organization repository

--- src/main/java/io/pakland/mdas/githubstats/infrastructure/github/repository/OrganizationGitHubRepository.java
+++ src/main/java/io/pakland/mdas/githubstats/infrastructure/github/repository/OrganizationGitHubRepository.java
@@ -5,10 +5,11 @@ import io.pakland.mdas.githubstats.application.mappers.OrganizationMapper;
 import io.pakland.mdas.githubstats.domain.entity.Organization;
 import io.pakland.mdas.githubstats.domain.repository.OrganizationExternalRepository;
 import io.pakland.mdas.githubstats.infrastructure.github.model.GitHubOrganizationDTO;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import java.util.List;
 
 public class OrganizationGitHubRepository implements OrganizationExternalRepository {
 
@@ -22,9 +23,10 @@ public class OrganizationGitHubRepository implements OrganizationExternalReposit
     @Override
     public List<Organization> fetchAvailableOrganizations() throws HttpException {
         try {
-            logger.info(" - Fetching organizations");
+            final String uri = "/user/orgs";
+            
             return this.webClientConfiguration.getWebClient().get()
-                .uri("/user/orgs")
+                .uri(uri)
                 .retrieve()
                 .bodyToFlux(GitHubOrganizationDTO.class)
                 .map(OrganizationMapper::dtoToEntity)
