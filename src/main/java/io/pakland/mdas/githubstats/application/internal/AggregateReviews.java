37952ab9ccdceb7c15640fa551dc6700418df401
sdomingobasora <sdomingobasora@gmail.com>
2022-12-27 01:18:38 +0100

    feat: add filtering by user

--- src/main/java/io/pakland/mdas/githubstats/application/internal/AggregateReviews.java
+++ src/main/java/io/pakland/mdas/githubstats/application/internal/AggregateReviews.java
@@ -2,9 +2,13 @@ package io.pakland.mdas.githubstats.application.internal;
 
 import io.pakland.mdas.githubstats.domain.entity.Review;
 import io.pakland.mdas.githubstats.domain.entity.ReviewAggregation;
+import io.pakland.mdas.githubstats.domain.entity.User;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class AggregateReviews {
@@ -12,8 +16,19 @@ public class AggregateReviews {
     ReviewAggregation reviewAggregation = new ReviewAggregation();
     public AggregateReviews() {}
 
-    public ReviewAggregation execute(List<Review> reviews) {
-        return reviewAggregation.aggregate(reviews);
+    public Map<User, ReviewAggregation> execute(List<Review> reviews) {
+        // { "userA": [reviewsFromA], "userB": [reviewsFromB] ... }
+        Map<User, List<Review>> groupedReviews = reviews.stream()
+            .collect(Collectors.groupingBy(Review::getUser));
+
+        // { "userA": aggregatedReviewsFromA, "userB": aggregatedReviewsFromB ... }
+        Map<User, ReviewAggregation> groupedAggReviews = new HashMap<>();
+        groupedReviews.keySet().forEach(user -> {
+            List<Review> reviewsByUser = groupedReviews.get(user);
+            groupedAggReviews.put(user, reviewAggregation.aggregate(reviewsByUser));
+        });
+
+        return groupedAggReviews;
     }
 
 }
