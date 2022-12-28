0a608cf2ff762051ba3c70853b9492cb775bbd14
sdomingobasora <sdomingobasora@gmail.com>
2022-12-28 13:20:29 +0100

    feat: add use case

---
+++ src/main/java/io/pakland/mdas/githubstats/application/internal/MergeAggregatesIntoMetrics.java
@@ -0,0 +1,16 @@
+package io.pakland.mdas.githubstats.application.internal;
+
+import io.pakland.mdas.githubstats.domain.entity.Metric;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeAggregatesIntoMetrics {
+
+    public List<Metric> execute(/* CommitAggregations, PullRequestAggregations, ReviewAggregations */) {
+        List<Metric> metrics = new ArrayList<>();
+
+        return metrics;
+    }
+
+}
