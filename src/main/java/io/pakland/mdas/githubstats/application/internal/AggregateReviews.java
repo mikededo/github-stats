767a3a3af56e745440ad40259da0f66880008aa1
Miquel de Domingo <miquelddg@gmail.com>
2022-12-28 17:17:26 +0100

    feat: keep structure inside aggregated reviews

--- src/main/java/io/pakland/mdas/githubstats/application/internal/AggregateReviews.java
+++ src/main/java/io/pakland/mdas/githubstats/application/internal/AggregateReviews.java
@@ -17,7 +17,7 @@ public class AggregateReviews {
     ReviewAggregation reviewAggregation = new ReviewAggregation();
     public AggregateReviews() {}
 
-    public Map<User, Map<Team, ReviewAggregation>> execute(List<Review> reviews) {
+    public Map<Team, Map<User, ReviewAggregation>> execute(List<Review> reviews) {
         // {
         //   "userA": {
         //      "teamA": [reviewsFromUserAInTeamA],
@@ -26,11 +26,9 @@ public class AggregateReviews {
         //   "userB": { "teamA": [reviewsFromUserBInTeamA] },
         //   "userC": { "teamB": [reviewsFromUserCInTeamB] } ...
         // }
-        Map<User, Map<Team, List<Review>>> groupedReviews = reviews.stream().collect(
-            Collectors.groupingBy(Review::getUser,
-            Collectors.groupingBy(review ->
-                review.getPullRequest().getRepository().getTeam()
-            )));
+        Map<Team, Map<User, List<Review>>> groupedReviews = reviews.stream().collect(
+            Collectors.groupingBy(review -> review.getUser().getTeam(),
+            Collectors.groupingBy(Review::getUser)));
 
         // {
         //   "userA": {
@@ -40,14 +38,14 @@ public class AggregateReviews {
         //   "userB": { "teamA": reviewAggregationUserBTeamA },
         //   "userC": { "teamB": reviewAggregationUserCTeamB } ...
         // }
-        Map<User, Map<Team, ReviewAggregation>> aggReviews = new HashMap<>();
-        groupedReviews.keySet().forEach(user -> {
-            Map<Team, ReviewAggregation> aggReviewsByUser = new HashMap<>();
-            groupedReviews.get(user).keySet().forEach(team -> {
-                List<Review> reviewsByUserByTeam = groupedReviews.get(user).get(team);
-                aggReviewsByUser.put(team, reviewAggregation.aggregate(reviewsByUserByTeam));
+        Map<Team, Map<User, ReviewAggregation>> aggReviews = new HashMap<>();
+        groupedReviews.keySet().forEach(team -> {
+            Map<User, ReviewAggregation> aggReviewsByUser = new HashMap<>();
+            groupedReviews.get(team).keySet().forEach(user -> {
+                List<Review> reviewsByUserByTeam = groupedReviews.get(team).get(user);
+                aggReviewsByUser.put(user, reviewAggregation.aggregate(reviewsByUserByTeam));
             });
-            aggReviews.put(user, aggReviewsByUser);
+            aggReviews.put(team, aggReviewsByUser);
         });
 
         return aggReviews;
