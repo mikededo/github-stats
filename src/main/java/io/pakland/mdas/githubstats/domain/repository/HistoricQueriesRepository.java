456df0f692053f9a99a0830fcff1e0bbf5286530
Paco Lozano <plozano@pakland.io>
2022-12-29 04:51:21 +0100

    refactor: remove unnecessary HistoricQueries

--- src/main/java/io/pakland/mdas/githubstats/domain/repository/HistoricQueriesRepository.java
+++
@@ -1,9 +0,0 @@
-package io.pakland.mdas.githubstats.domain.repository;
-
-import io.pakland.mdas.githubstats.domain.HistoricQueries;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HistoricQueriesRepository extends JpaRepository<HistoricQueries, Integer> {
-}
