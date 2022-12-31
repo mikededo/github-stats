17f8e11cb681c801947de2d4767c231639894507
Paco Lozano <plozano@pakland.io>
2022-12-31 20:12:30 +0100

    refactor: some fixes

--- src/main/java/io/pakland/mdas/githubstats/GithubStatsApplication.java
+++ src/main/java/io/pakland/mdas/githubstats/GithubStatsApplication.java
@@ -2,10 +2,11 @@ package io.pakland.mdas.githubstats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("io.pakland.mdas.githubstats.domain.repository")
+@EnableJpaRepositories(basePackages = {"io.pakland.mdas.githubstats.domain.repository", "io.pakland.mdas.githubstats.infrastructure.postgres.repository"})
 public class GithubStatsApplication {
   public static void main(String[] args) {
     SpringApplication.run(GithubStatsApplication.class, args);
--- src/main/java/io/pakland/mdas/githubstats/domain/repository/impl/MetricRepositoryCustomImpl.java
+++ src/main/java/io/pakland/mdas/githubstats/domain/repository/impl/MetricRepositoryCustomImpl.java
@@ -3,7 +3,6 @@ package io.pakland.mdas.githubstats.domain.repository.impl;
 import io.pakland.mdas.githubstats.domain.Metric;
 import io.pakland.mdas.githubstats.domain.repository.MetricRepositoryCustom;
 import io.pakland.mdas.githubstats.infrastructure.postgres.model.PostgresOptionRequest;
-import io.pakland.mdas.githubstats.infrastructure.shell.model.ShellRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -13,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Transactional(readOnly=true)
-public class MetricRepositoryImpl implements MetricRepositoryCustom {
+public class MetricRepositoryCustomImpl implements MetricRepositoryCustom {
 
     @PersistenceContext
     EntityManager entityManager;
@@ -71,44 +70,4 @@ public class MetricRepositoryImpl implements MetricRepositoryCustom {
                 .getResultList();
     }
 
-    /*@Transactional(readOnly=true)
-    @Query(nativeQuery = true, value="""
-            SELECT *
-              FROM metric
-             WHERE organization = :#{#metric.organization}
-               AND team_slug = :#{#metric.teamSlug}
-               AND user_name = :#{#metric.userName}
-               AND year_month = to_date(:#{#metric.yearMonth.toString()}, 'YYYY-MM')
-    """)
-    Metric getMetricUsingUniqueConstraint(@Param("metric") Metric metric);
-
-    @Transactional(readOnly=true)
-    @Query(nativeQuery = true, value= """
-           SELECT COUNT(*)
-           FROM (
-                 SELECT COUNT(*)
-                   FROM metric
-                  WHERE user_name = :#{#req.name}
-                    AND year_month 
-                        BETWEEN to_date(:#{#req.dateFrom.toString()}, 'YYYY-MM')
-                        AND to_date(:#{#req.dateTo.toString()}, 'YYYY-MM')
-               GROUP BY user_name,
-                        EXTRACT(YEAR from year_month), 
-                        EXTRACT(MONTH from year_month)
-           ) as sub
-    """)
-    Long countUserMonthsFromRange(@Param(value="req") ShellRequest req);
-
-    @Transactional(readOnly=true)
-    @Query(nativeQuery = true, value= """
-             SELECT *
-               FROM metric
-              WHERE user_name = :#{#req.name}
-                AND year_month 
-                    BETWEEN to_date(:#{#req.dateFrom.toString()}, 'YYYY-MM') 
-                    AND to_date(:#{#req.dateTo.toString()}, 'YYYY-MM')
-           ORDER BY user_name, year_month
-    """)
-    List<Metric> findUserMetricsFromRange(@Param(value="req") ShellRequest req);
-    */
 }
