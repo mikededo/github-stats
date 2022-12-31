d205fd738dc1e3edc46820ce4f39504ac15689cd
Paco Lozano <plozano@pakland.io>
2022-12-31 10:56:44 +0100

    refactor: remove HistoricQueries

--- src/main/java/io/pakland/mdas/githubstats/domain/repository/MetricRepository.java
+++ src/main/java/io/pakland/mdas/githubstats/domain/repository/MetricRepository.java
@@ -1,9 +1,9 @@
 package io.pakland.mdas.githubstats.domain.repository;
 
-import io.pakland.mdas.githubstats.domain.HistoricQueries;
+import io.pakland.mdas.githubstats.domain.Metric;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MetricRepository extends JpaRepository<HistoricQueries, Integer> {
+public interface MetricRepository extends JpaRepository<Metric, Integer> {
 }
